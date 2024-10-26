f30586dc52ac15f67eff26ae6d1a13c68cc4a1e3
x <phamhuyen19124@gmail.com>
2024-10-26 11:08:03 +0700

    fisrt tree

--- src/components/TreeComponent.vue
+++ src/components/TreeComponent.vue
@@ -7,7 +7,10 @@
           :class="isOpen(node) ? 'fas fa-minus' : 'fas fa-plus'"
         ></i>
       </span>
-      <span class="label">{{ node.name }}</span>
+      <span class="label">
+        <i :class="node.children && node.children.length ? 'fas fa-folder' : 'fas fa-file-alt'" class="icon"></i>
+        {{ node.name }}
+      </span>
       <tree-component
         v-if="node.children && node.children.length && isOpen(node)"
         :tree="node.children"
@@ -28,15 +31,13 @@ const openNodes = ref([]);
 
 // Hàm để kiểm tra node đang mở hay đóng
 const isOpen = (node) => openNodes.value.includes(node.id);
-
-
-
+console.log(openNodes)
 // Hàm để thay đổi trạng thái mở/đóng của node
 const toggle = (node) => {
   if (isOpen(node)) {
-    openNodes.value = openNodes.value.filter((id) => id !== node.id); //tạo một mảng mới, loại bỏ node.id khỏi openNodes.value, giúp đóng node lại.
+    openNodes.value = openNodes.value.filter((id) => id !== node.id); // loại bỏ node.id khỏi openNodes.value
   } else {
-    openNodes.value.push(node.id); // nếu node đag đóng thì thêm id vào mảng để mở ra
+    openNodes.value.push(node.id); // thêm id vào mảng để mở node
   }
 };
 </script>
@@ -48,6 +49,9 @@ ul {
 }
 .toggle-icon {
   cursor: pointer;
+  margin-right: 5px;
+}
+.node-icon {
   margin-right: 5px;
 }
 </style>
